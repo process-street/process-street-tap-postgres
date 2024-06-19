a43b2cc7f56b74c91e3cc8c0730eb77e4be70850
Cameron McKay <me@cdmckay.org>
2024-06-19 12:24:42 +0200

    Add support for interval type

--- tap_postgres/discovery_utils.py
+++ tap_postgres/discovery_utils.py
@@ -218,6 +218,10 @@ def schema_for_column_datatype(col):
         schema['type'] = nullable_column('string', col.is_primary_key)
         return schema
 
+    if data_type == 'interval':
+        schema['type'] = nullable_column('string', col.is_primary_key)
+        return schema
+
     if data_type == 'ltree':
         schema['type'] = nullable_column('string', col.is_primary_key)
         return schema
